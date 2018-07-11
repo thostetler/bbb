2e15dcfa07d968c6f6452545325a49ce773cb47b
Tim Hostetler <timothy.hostetler@cfa.harvard.edu>
2018-07-11 17:00:56 -0400

    update

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,25 +2,44 @@ import '../static/scss/main.scss';
 import { h, Component } from 'preact';
 import { Route, Link } from 'react-router-dom';
 import Nav from './components/nav/nav.jsx';
-import Section from './components/section/section.jsx';
+
+const ResultsLayout = () =>
+<div className="columns">
+  <div className="column has-background-white-ter">
+    LEFT
+  </div>
+  <div className="column is-half has-background-white-ter">
+    Main
+  </div>
+  <div className="column has-background-white-ter">
+    Right
+  </div>
+</div>
+
+const LandingLayout = () =>
+<div className="container is-fluid">
+  LANDING
+</div>
+
+const Breadcrumb = () =>
+<nav className="breadcrumb" aria-label="breadcrumbs">
+  <ul>
+    <li><Link to="/">Home</Link></li>
+    <li className="is-active">
+      <Link to="#" aria-current="page">Search</Link>
+    </li>
+  </ul>
+</nav>
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Nav/>
+        <Breadcrumb/>
         <section className="section">
-          <div className="columns">
-            <div className="column has-background-white-ter">
-              A
-            </div>
-            <div className="column is-half has-background-white-ter">
-              B
-            </div>
-            <div className="column has-background-white-ter">
-              C
-            </div>
-          </div>
+          <Route exact path="/" component={LandingLayout} />
+          <Route path="/search" component={ResultsLayout} />
         </section>
       </div>
     );
